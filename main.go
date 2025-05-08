51ac4354c6f81f97a5d85815f54ef44bdf3f53d7
BIGboss248 <aminjamali79@gmail.com>
2025-05-08 08:32:59 +0330

    Added a scraper

--- main.go
+++ main.go
@@ -11,23 +11,87 @@
 //To generate a binary file
 //$ go build <gofile>
 
-// Simple web scraping example using Colly and XPath
+// Package declaration
 package main
 
+// import packages
 import (
 	"fmt"
-	"sync"
+	"os"
 	"time"
 
 	"github.com/gocolly/colly"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
-func scrapeData(xpath string, url string) (*colly.XMLElement, error) {
+// ANSI color codes for terminal output
+const (
+	Reset = "\033[0m"
+	Bold  = "\033[1m"
+	// Text Colors
+	FgBlack   = "\033[30m"
+	FgRed     = "\033[31m"
+	FgGreen   = "\033[32m"
+	FgYellow  = "\033[33m"
+	FgBlue    = "\033[34m"
+	FgMagenta = "\033[35m"
+	FgCyan    = "\033[36m"
+	FgWhite   = "\033[37m"
+	// Background Colors
+	BgBlack   = "\033[40m"
+	BgRed     = "\033[41m"
+	BgGreen   = "\033[42m"
+	BgYellow  = "\033[43m"
+	BgBlue    = "\033[44m"
+	BgMagenta = "\033[45m"
+	BgCyan    = "\033[46m"
+	BgWhite   = "\033[47m"
+)
 
-	// scrapeData scrapes data from a webpage using the provided XPath and URL.
-	// It initializes a new Colly collector, listens for elements matching the XPath,
-	// and returns the first matched element or an error if the scraping fails.
-	//
+// SetupLogger initializes zerolog to write to both console and a file.
+func SetupLogger(logFilePath string, level zerolog.Level) (zerolog.Logger, error) {
+	//* Import these packages
+	//* "os"
+	//* "time"
+	//* "github.com/rs/zerolog"
+	//* "github.com/rs/zerolog/log"
+	// Open or create the log file
+	file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o644)
+	if err != nil {
+		return zerolog.Logger{}, err
+	}
+	fileLogger := zerolog.New(file)
+	// Console writer with human-friendly formatting
+	consoleWriter := zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: time.RFC3339,
+	}
+
+	// Combine both writers
+	multi := zerolog.MultiLevelWriter(consoleWriter, fileLogger)
+
+	// Set global time format
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	// Set log level
+	zerolog.SetGlobalLevel(level)
+
+	// Create the logger
+	logger := zerolog.New(multi).With().Timestamp().Caller().Logger()
+
+	// Set as the global logger
+	log.Logger = logger
+
+	return logger, nil
+}
+
+/*
+ScrapeDataXpath scrapes data from a webpage using the provided XPath and URL.
+It initializes a new Colly collector, listens for elements matching the XPath,
+and returns the first matched element or an error if the scraping fails.
+*/
+func ScrapeDataXpath(xpath string, url string) (*colly.XMLElement, error) {
 	//* Make sure to import the necessary packages:
 	//*	github.com/gocolly/colly
 	//
@@ -49,38 +113,17 @@ func scrapeData(xpath string, url string) (*colly.XMLElement, error) {
 	return element, nil
 }
 
+// The function that will be executed
 func main() {
-	start := time.Now() // Record the start time
-
-	var wg sync.WaitGroup // Waitgroup default value is 0
-	// Add a count of 1 to the WaitGroup
-	wg.Add(1) // So now we have 1 goroutine to wait for
-	go func() {
-		defer wg.Done() // Decrement the counter when the goroutine completes
-		element, err := scrapeData("/html/body/main/div[1]/div[1]/div[1]/div/div[2]/div/h3[1]/span[2]/span[1]", "https://www.tgju.org/profile/geram24")
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-		fmt.Println("Element text:", element.Text)
-	}()
-
-	wg.Add(1) // Add another goroutine to wait format
-	go func() {
-		defer wg.Done() // Decrement the counter when the goroutine completes
-		element, err := scrapeData("/html/body/main/div[1]/div[1]/div[1]/div/div[2]/div/h3[1]/span[2]/span[1]", "https://www.tgju.org/profile/price_dollar_rl")
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-		fmt.Println("Element text:", element.Text)
-	}()
-
-	// Wait for the goroutine to finish
-	wg.Wait() // Will block until the WaitGroup counter is 0
-
-	fmt.Println("\033[32mAll goroutines using wait group finished!\033[0m") // Print the message to the console
-
-	elapsed := time.Since(start) // Calculate the elapsed time
-	fmt.Printf("\033[33;4mExecution time: %s\033[0m\n", elapsed)
+	logger, err := SetupLogger("app.log", zerolog.InfoLevel)
+	if err != nil {
+		panic(err)
+	}
+	logger.Info().Msg(FgCyan + "Main function started" + Reset)
+	defer logger.Info().Msg(FgCyan + "Main function ended" + Reset)
+	element, err := ScrapeDataXpath("/html/body/main/div[1]/div[1]/div[1]/div/div[2]/div/h3[1]/span[2]/span[1]", "https://www.tgju.org/profile/price_dollar_rl")
+	if err != nil {
+		log.Error().Err(err)
+	}
+	fmt.Println(element.Text)
 }
