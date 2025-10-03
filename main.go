9f87b01bb810748589691eaf9de65b0f7fcfba6a
BIGboss248 <aminjamali79@gmail.com>
2025-10-03 22:32:52 +0330

    Started a new project as an effort to learn golang and how creating a software works

--- main.go
+++ main.go
@@ -1,231 +1,23 @@
-// Package declaration
 package main
 
 // import packages
 import (
-	"fmt"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"
-	"sync"
 	"time"
 
-	"github.com/gocolly/colly"
+	clog "github.com/BIGboss248/golang-practice/utils/clog"
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
-// ANSI color codes for terminal output
-const (
-	Reset = "\033[0m"
-	Bold  = "\033[1m"
-	// Text Colors
-	FgBlack   = "\033[30m"
-	FgRed     = "\033[31m"
-	FgGreen   = "\033[32m"
-	FgYellow  = "\033[33m"
-	FgBlue    = "\033[34m"
-	FgMagenta = "\033[35m"
-	FgCyan    = "\033[36m"
-	FgWhite   = "\033[37m"
-	// Background Colors
-	BgBlack   = "\033[40m"
-	BgRed     = "\033[41m"
-	BgGreen   = "\033[42m"
-	BgYellow  = "\033[43m"
-	BgBlue    = "\033[44m"
-	BgMagenta = "\033[45m"
-	BgCyan    = "\033[46m"
-	BgWhite   = "\033[47m"
-)
-
-func HTTPHandler(w http.ResponseWriter, r *http.Request) {
-	HTTPServerStartTime := time.Now() // Record start time
-	logger, err := SetupLogger("app.log", zerolog.InfoLevel)
-	if err != nil {
-		panic(err)
-	}
-	logger.Info().Msg(FgCyan + "HTTPServer function started" + Reset)
-	defer func() {
-		HTTPServerDuration := time.Since(HTTPServerStartTime)
-		logger.Info().Msgf(FgCyan+"HTTPServer function ended. Execution time: %s"+Reset, HTTPServerDuration)
-	}()
-	//Code here
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-func HTTPServer(handler func(http.ResponseWriter, *http.Request), networkInterface string, path string) {
-	HTTPServerStartTime := time.Now() // Record start time
-	// defer keyword will execute a code when the function returns
-	logger, err := SetupLogger("app.log", zerolog.InfoLevel)
-	if err != nil {
-		panic(err)
-	}
-	logger.Info().Msg(FgCyan + "HTTPServer function started" + Reset)
-	defer func() {
-		HTTPServerDuration := time.Since(HTTPServerStartTime)
-		logger.Info().Msgf(FgCyan+"HTTPServer function ended. Execution time: %s"+Reset, HTTPServerDuration)
-	}()
-	//Code here
-	http.HandleFunc(path, handler)
-	httpErr := http.ListenAndServe(networkInterface, nil)
-	if httpErr != nil {
-		log.Err(httpErr).Msg("")
-	}
-}
-
-
-/*
-SetupLogger initializes zerolog to write to both console and a file.
-It sets up logging to both the console and a file, allowing for both real-time monitoring and persistent record-keeping.
-
-Parameters:
-
-- logFilePath: The path to the log file where logs will be written.
-
-- logLevel: The minimum level of logs to be written (e.g., DebugLevel, InfoLevel, ErrorLevel).
-
-Returns:
-
-- zerolog.Logger: The configured logger instance.
-
-- error: An error if the log file cannot be opened or created.
-*/
-func SetupLogger(logFilePath string, logLevel zerolog.Level) (zerolog.Logger, error) {
-	// Open or create the log file
-	file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o644)
-	if err != nil {
-		return zerolog.Logger{}, err
-	}
-
-	// Console writer with human-friendly formatting
-	consoleWriter := zerolog.ConsoleWriter{
-		Out:        os.Stdout,
-		TimeFormat: time.RFC3339,
-	}
-
-	// Set global log level
-	zerolog.SetGlobalLevel(logLevel)
-
-	// Combine both writers
-	multi := zerolog.MultiLevelWriter(consoleWriter, file)
-
-	// Set global time format
-	zerolog.TimeFieldFormat = time.RFC3339
-
-	// Create the logger
-	logger := zerolog.New(multi).With().Caller().Timestamp().Logger()
-
-	// Set as the global logger
-	log.Logger = logger
-
-	return logger, nil
-}
-
-/*
-ScrapeDataXpath scrapes data from a webpage using the provided XPath and URL.
-It initializes a new Colly collector, listens for elements matching the XPath,
-and returns the first matched element or an error if the scraping fails.
-
-Parameters:
-
-- xpath: A string representing the XPath query to locate elements on the webpage.
-
-- url: A string representing the URL of the webpage to scrape.
-
-Returns:
-
-- *colly.XMLElement: A pointer to the first matched XMLElement.
-
-- error: An error if the scraping process encounters an issue.
-*/
-func ScrapeDataXpath(xpath string, url string) (*colly.XMLElement, error) {
-	// Create a new collector
-	c := colly.NewCollector()
-	element := &colly.XMLElement{}
-	c.OnXML(xpath, func(e *colly.XMLElement) {
-		element = e
-	})
-	c.Visit(url)
-	return element, nil
-}
-
-type currency struct {
-	xpath string
-	url   string
-	name  string
-} // access members by currency.xpath
 // The function that will be executed
 func main() {
+	logger, err := clog.SetupLogger("app.log", zerolog.InfoLevel)
 	startTime := time.Now() // Record start time
-	logger, err := SetupLogger("app.log", zerolog.InfoLevel)
 	if err != nil {
 		panic(err)
 	}
-	logger.Info().Msg(FgCyan + "Main function started" + Reset)
-	// defer logger.Info().Msg(FgCyan + "Main function ended" + Reset)
+	logger.Debug().Str("FunctionName:", "main").Msg("Main function started")
 	defer func() {
-		duration := time.Since(startTime)
-		logger.Info().Msgf(FgCyan+"Main function ended. Execution time: %s"+Reset, duration)
+		logger.Debug().Str("FunctionName:", "main").TimeDiff("Duration (ms)", time.Now(), startTime).Msg("Main function ended.")
 	}()
 
-	xpath := "/html/body/main/div[1]/div[1]/div[1]/div/div[2]/div/h3[1]/span[2]/span[1]"
-	// Create currency list
-	currencies := []currency{}
-	var currencyMap = map[string]float64{}
-	USD := currency{
-		name:  "USD",
-		xpath: xpath,
-		url:   "https://www.tgju.org/profile/price_dollar_rl",
-	}
-	EUR := currency{
-		name:  "EUR",
-		xpath: xpath,
-		url:   "https://www.tgju.org/profile/price_eur",
-	}
-	AED := currency{
-		name:  "AED",
-		xpath: xpath,
-		url:   "https://www.tgju.org/profile/price_aed",
-	}
-	GoldOunce := currency{
-		name:  "GoldOunce",
-		xpath: xpath,
-		url:   "https://www.tgju.org/profile/ons",
-	}
-	currencies = append(currencies, USD)
-	currencies = append(currencies, EUR)
-	currencies = append(currencies, AED)
-	currencies = append(currencies, GoldOunce)
-
-	for _, value := range currencies {
-		// Create a WaitGroup to wait for goroutines to finish
-		var wg sync.WaitGroup // Waitgroup default value is 0
-		// Add a count of 1 to the WaitGroup
-		wg.Add(1) // So now we have 1 goroutine to wait for
-		go func() {
-			defer wg.Done() // Decrement the counter when the goroutine completes
-			element, err := ScrapeDataXpath(value.xpath, value.url)
-			if err != nil {
-				log.Err(err)
-			} else {
-				// Remove , from element.text
-				text := element.Text
-				// Remove comma from the text
-				text = strings.ReplaceAll(text, ",", "")
-				// Convert to float
-				floatValue, err := strconv.ParseFloat(text, 64)
-				if err != nil {
-					log.Panic().Err(err).Msg("")
-				}
-				currencyMap[value.name] = floatValue
-				log.Info().Msgf("Value of %s is %f", value.name, floatValue)
-			}
-		}()
-
-		// Wait for the goroutine to finish
-		wg.Wait() // Will block until the WaitGroup counter is 0
-	}
-	fmt.Println(currencyMap)
-	HTTPServer(HTTPHandler,":8080","/")
 }
